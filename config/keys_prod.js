78c8d2993a5772afcdcf61ed3ff5ddfba804c1ef
DakotaHall <37163652+DakotaHall@users.noreply.github.com>
2021-03-16 19:11:49 -0500

    Delete keys_prod.js

--- config/keys_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://dakotahall5:<password>@dmkit.ymvao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: process.env.SECRET_OR_KEY
-};
